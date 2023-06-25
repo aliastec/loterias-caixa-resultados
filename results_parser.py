ad574ffbc0b90aa072b650d76795ea5a1fb35e96
aliastec <137111438+aliastec@users.noreply.github.com>
2023-06-25 16:13:33 +0000

    Fixed all pylint errors. Included output folders.

--- results_parser.py
+++ results_parser.py
@@ -1,32 +1,37 @@
+"""Parse a loto HTML page to a CSV."""
+
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 
 # Parse results page to comma-separated values.
 def parse_results_page(page_source):
+    """Parse the given HTML to CSV"""
     soup = BeautifulSoup(page_source, 'html.parser')
 
     csv_contents = ''
     rows = soup.find('table', class_='tabela-resultado').contents
     for row in rows:
         line = ''
-        if isinstance(row, Tag) and row.tr != None:
+        if isinstance(row, Tag) and row.tr is not None:
             cells = row.tr.contents
             for cell in cells:
-                if cell.string != None:
+                if cell.string is not None:
                     line += '{},'.format(cell.string.strip('\n').replace(',', ''))
-                elif len(cell.contents) > 0 and cell.table != None:
+                elif len(cell.contents) > 0 and cell.table is not None:
                     values_array = '('
                     subrows = cell.table.tbody.contents
                     for subrow in subrows:
                         if isinstance(subrow, Tag):
                             subcells = subrow.contents
                             for subcell in subcells:
-                                if subcell.string != None and len(subcell.string.strip()) > 0:
-                                    values_array += '[{}]'.format(subcell.string.strip('\n').replace(',', ''))
+                                if subcell.string is not None and len(subcell.string.strip()) > 0:
+                                    values_array += '[{}]'.format(
+                                        subcell.string.strip('\n').replace(',', '')
+                                    )
                     values_array += ')'
-                    line += '{},'.format(values_array)
+                    line += f'{values_array},'
                 else:
                     line += ','
-            csv_contents += '{}\n'.format(line.rstrip(','))
+            csv_contents += line.rstrip(',') + '\n'
 
     return csv_contents.replace('.', '').replace('R$', '')
